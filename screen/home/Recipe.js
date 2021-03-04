b62513cec4d127b73cbbafb1cf2248b81d58f604
dannyhannyford <dannyhannyford@gmail.com>
2021-03-04 15:36:01 -0800

    recipe cards pass navigationId

--- screen/home/Recipe.js
+++ screen/home/Recipe.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Recipe({ navigation }) {
+export default function Recipe({ route, navigation }) {
+  const { recipeId } = route.params;
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Recipe</Text>
+      <Text>Recipe: {recipeId}</Text>
     </View>
   );
 }
--- screen/home/RecipeCard.js
+++ screen/home/RecipeCard.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
-export default function RecipeCard({ navigation, recipeInfo }) {
+export default function RecipeCard({ recipeInfo }) {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>RecipeCard: {recipeInfo.userName}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Recipe', { recipeId: recipeInfo.id})}
+      >
+        <Text>Recipe Card: {recipeInfo.id}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
+});
+
