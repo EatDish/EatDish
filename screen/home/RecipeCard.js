ac17d4c0a2300ab2ec2bfde3bc4757c6081e6715
Kyle Cordell <kcordell-git@outlook.com>
2021-03-04 20:09:32 -0600

    Recipe card populated with data and preliminary formatting

--- screen/home/RecipeCard.js
+++ screen/home/RecipeCard.js
@@ -5,12 +5,20 @@ import { useNavigation } from '@react-navigation/native';
 export default function RecipeCard({ recipeInfo }) {
   const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Recipe', { recipeId: recipeInfo.id})}
       >
-        <Text>Recipe Card: {recipeInfo.id}</Text>
+        <View style={styles.infoRow} >
+          {/* //TODO: ICON PLACEHOLDER */}
+          <Text style={styles.dishName}>{recipeInfo.dishName}</Text>
+          <Text style={styles.cuisine}>{recipeInfo.cuisine}</Text>
+        </View>
+        <View style={styles.infoRow} >
+          <Text style={styles.detail}>{`Prep time: ${recipeInfo.prepTime} mins`}</Text>
+          <Text style={styles.detail}>{`Cook time: ${recipeInfo.cookTime} mins`}</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -20,17 +28,39 @@ export default function RecipeCard({ recipeInfo }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 10
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
   button: {
-    alignItems: "center",
     backgroundColor: "#DDDDDD",
-    padding: 10
+    padding: 10,
+    borderBottom: "1px solid #FFF"
   },
-  countContainer: {
+  infoRow: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignContent: "flex-start",
     alignItems: "center",
-    padding: 10
+    paddingLeft: 10
+  },
+  dishName: {
+    fontWeight: 600,
+    fontSize: 18,
+    paddingRight: 10,
+    textTransform: "capitalize"
+  },
+  cuisine: {
+    fontWeight: 400,
+    fontSize: 17,
+    fontStyle: "italic",
+    flexGrow: "auto",
+    textTransform: "capitalize"
+  },
+  detail: {
+    fontWeight: 400,
+    fontSize: 14,
+    paddingRight: 10
   }
 });
 
