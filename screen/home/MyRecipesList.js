baac71e25a22bd395bd978391c56176405df8269
dannyhannyford <dannyhannyford@gmail.com>
2021-03-16 20:00:42 -0700

    MyRecipeList cleanup

--- screen/home/MyRecipesList.js
+++ screen/home/MyRecipesList.js
@@ -38,7 +38,6 @@ export default function MyRecipesList() {
     try {
       const recipeData = await API.graphql(graphqlOperation(customQuery))
       const recipes = recipeData.data.listRecipes.items
-      console.log('MyRecipesList.js -- recipeData.data:', recipeData.data);
       console.log('MyRecipesList.js -- recipes:', recipes);
       setRecipes(recipes)
     } catch (err) { console.log('error fetching recipes', err) }
