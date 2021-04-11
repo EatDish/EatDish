f6fbdea4ca499549ef81c90cd330f4d23ed7f6a6
dannyhannyford <dannyhannyford@gmail.com>
2021-04-10 22:26:39 -0700

    query for user login currently returns userID if user/pass matches

--- src/navigation/AccountStackScreen.js
+++ src/navigation/AccountStackScreen.js
@@ -3,12 +3,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Account from "../screens/account/Account";
 import Settings from "../screens/account/Settings";
 import Appearance from "../screens/account/Appearance";
+import Register from "../screens/auth/Register";
+import Login from "../screens/auth/Login";
 
 const AccountStack = createStackNavigator();
 
 export default function AccountStackScreen({ navigation }) {
 	return (
 		<AccountStack.Navigator>
+			<AccountStack.Screen name="Login" component={Login} />
+			<AccountStack.Screen name="Register" component={Register} />
 			<AccountStack.Screen name="Account" component={Account} />
 			<AccountStack.Screen name="Settings" component={Settings} />
 			<AccountStack.Screen name="Appearance" component={Appearance} />
