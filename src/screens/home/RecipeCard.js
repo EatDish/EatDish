5061cf4f4609c1f044ffe9ad8503be93ca1bc6e5
dannyhannyford <dannyhannyford@gmail.com>
2021-06-08 17:19:34 -0700

    Recipe Card paper rework with image and wanted info

--- src/screens/home/RecipeCard.js
+++ src/screens/home/RecipeCard.js
@@ -1,92 +1,29 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { useNavigation, useTheme } from '@react-navigation/native';
+import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 export default function RecipeCard({ recipeInfo }) {
-  const { colors } = useTheme();
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={{
-          backgroundColor: colors.card,
-          padding: 10,
-          margin: 3,
-          color: colors.text
-        }}
-        onPress={() => navigation.navigate('Recipe', { recipe: recipeInfo })}>
-        <View style={styles.infoRow}>
-          <Text
-            style={{
-              fontWeight: '500',
-              fontSize: 18,
-              paddingRight: 10,
-              textTransform: 'capitalize',
-              color: colors.text
-            }}>
-            {recipeInfo.dishName}
-          </Text>
-          <Text
-            style={{
-              fontWeight: '400',
-              fontSize: 17,
-              fontStyle: 'italic',
-              textTransform: 'capitalize',
-              color: colors.text
-            }}>
-            {recipeInfo.cuisine}
-          </Text>
-        </View>
-        <View style={styles.infoRow}>
-          <Text
-            style={{
-              fontWeight: '400',
-              fontSize: 14,
-              paddingRight: 10,
-              color: colors.text
-            }}>{`Prep time: ${recipeInfo.prepTime} mins`}</Text>
-          <Text
-            style={{
-              fontWeight: '400',
-              fontSize: 14,
-              paddingRight: 10,
-              color: colors.text
-            }}>{`Cook time: ${recipeInfo.cookTime} mins`}</Text>
-        </View>
+    <Card>
+      <TouchableOpacity onPress={() => navigation.navigate('Recipe', { recipe: recipeInfo })}>
+        <Card.Cover
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK1bPJyZFSVNJArocOLzfbkRMC5_GsbZFUfw&usqp=CAU'
+          }}
+        />
+        <Card.Title title={recipeInfo.dishName} subtitle={recipeInfo.cuisine} />
+        <Card.Content>
+          <Paragraph>
+            {recipeInfo.prepTime ? recipeInfo.prepTime + recipeInfo.cookTime : recipeInfo.cookTime}{' '}
+            minutes
+          </Paragraph>
+          <Paragraph>Forks</Paragraph>
+          <Paragraph>Stars</Paragraph>
+          <Paragraph>User</Paragraph>
+        </Card.Content>
       </TouchableOpacity>
-    </View>
+    </Card>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    paddingHorizontal: 2
-  },
-  infoRow: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignContent: 'flex-start',
-    alignItems: 'center',
-    paddingLeft: 10
-  },
-  dishName: {
-    fontWeight: '500',
-    fontSize: 18,
-    paddingRight: 10,
-    textTransform: 'capitalize'
-  },
-  cuisine: {
-    fontWeight: '400',
-    fontSize: 17,
-    fontStyle: 'italic',
-    textTransform: 'capitalize'
-  },
-  detail: {
-    fontWeight: '400',
-    fontSize: 14,
-    paddingRight: 10
-  }
-});
